21b354824a5dbc20f6fee2d28dfe7188012d3672
Cauak <caua.crosati@edu.unifil.br>
2024-11-22 20:03:30 -0300

    update final calculadora

--- cod.py
+++ cod.py
@@ -73,4 +73,23 @@ while True:
            for item in historico:
                print(item)
        else:
-           print("Nenhum cálculo realizado ainda."
+           print("Nenhum cálculo realizado ainda.")
+     elif menu == 0:
+        print("Saindo do software...")
+        break
+
+    else:
+        print("Opção invalida, encerrando programa...")
+        break
+
+    print()
+    print("Deseja continuar utilizando o software?")
+    print("1 - Sim")
+    print("2 - Não")
+    continuar = int(input("Escolha a opção desejada: "))
+    
+    if continuar == 1:
+        limpar_tela()
+    else:
+        print("Encerrando o programa...")
+        break
